322623f99e6d650c0aa626bf690961cd69308a6a
Siddartha Datta <sid2002official@gmail.com>
2025-02-08 22:09:57 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import connectDB from './config/db.js';
 import recipeRoutes from './routes/recipesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import protectedRoutes from './routes/protectedRoutes.js';
@@ -13,22 +16,24 @@ connectDB();
 
 const app = express();
 
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({ limit:'10mb',extended: true }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
   origin: 'https://recipe-manager-lbkl.onrender.com',  // specify your frontend URL here
   credentials: true,  // allow cookies to be sent with the request
 }));
 app.use(cookieParser());
 
-
+// Serve static files
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
-
 app.use('/api/user', userRoutes);
 app.use('/api', protectedRoutes);
-
 
 app.get('/', (req, res) => {
   res.send("Server is running");
